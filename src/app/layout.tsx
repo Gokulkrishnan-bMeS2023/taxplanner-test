7db9ebc915c4d3cbb6a602d4e23d4863eef61e3c
Gokulkrishnan47 <gokulkrishnan20092002@gmail.com>
2024-04-02 12:09:28 +0530

    overall component update4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,24 @@
 import { Providers } from "./providers";
 import dynamic from "next/dynamic";
 import UserContextProvider from "../utils/context/index";
+const Footer = dynamic(() => import("@/components/Footer"));
+const ScrollToTopButton = dynamic(() => import("@/components/BackToTopButton"));
 import "./globals.css";
-import { useUserContext } from "@/utils/hooks";
-import Loading from "./loading";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const Footer = dynamic(() => import("@/components/Footer"));
-  const ScrollToTopButton = dynamic(
-    () => import("@/components/BackToTopButton")
-  );
-
-  const { data } = useUserContext();
-
   return (
     <html lang="en">
       <body>
         <UserContextProvider>
-          {data ? (
-            <>
-              <Providers>
-                <main>{children}</main>
-                <Footer />
-                <ScrollToTopButton />
-              </Providers>
-            </>
-          ) : (
-            Loading
-          )}
+          <Providers>
+            <main>{children}</main>
+            <Footer />
+            <ScrollToTopButton />
+          </Providers>
         </UserContextProvider>
       </body>
     </html>
--- src/app/salary-house-property/page.tsx
+++ src/app/salary-house-property/page.tsx
@@ -3,25 +3,57 @@ import { Box, Container } from "@chakra-ui/react";
 import DocumentSubmitted from "@/components/DocumentSubmitted";
 import AboutThisPlan from "@/components/AboutThisPlan";
 import ContentWithPriceCard from "@/components/ContentWithPriceCard";
-import { AboutThisPlanCardContent, AboutThisPlanContent } from "@/component-contents/incometax-Salary-house-property/AboutThisPlan";
+import {
+  AboutThisPlanCardContent,
+  AboutThisPlanContent,
+} from "@/component-contents/incometax-Salary-house-property/AboutThisPlan";
 import { Documents } from "@/component-contents/incometax-Salary-house-property/DocumentSubmitted";
 import { ContentWithPriceCardContents } from "@/component-contents/incometax-Salary-house-property/ContentWithPriceCard";
 import FAQSection from "@/components/FAQSection";
 import { FAQs } from "@/component-contents/incometax-Salary-house-property/FAQSection";
 import BGWithQueriesForm from "@/components/BGWithQueriesForm";
+import { useUserContext } from "@/utils/hooks";
+import Loading from "../loading";
 
 export default function SalaryHouseProperty() {
+  const { data } = useUserContext();
   return (
     <>
-      <Container>
-      <Box my={{ base: 16, lg: 24 }}><ContentWithPriceCard contents={ContentWithPriceCardContents} FilingType="SalariedPerson"/></Box>
-      <Box my={{ base: 16, lg: 24 }}><AboutThisPlan title="About This Plan" planContents={AboutThisPlanCardContent} aboutContents={AboutThisPlanContent}/></Box> 
-      <Box my={{ base: 16, lg: 24 }}><DocumentSubmitted title="Salary & House Property" documents={Documents}  /></Box>
-      <Box my={{ base: 16, lg: 24 }}><FAQSection
-            heading="Frequently Asked Questions"
-            servicesDatas={FAQs} /></Box>
-      </Container>
-      <BGWithQueriesForm />
+      {data ? (
+        <>
+          {" "}
+          <Container>
+            <Box my={{ base: 16, lg: 24 }}>
+              <ContentWithPriceCard
+                contents={ContentWithPriceCardContents}
+                FilingType="SalariedPerson"
+              />
+            </Box>
+            <Box my={{ base: 16, lg: 24 }}>
+              <AboutThisPlan
+                title="About This Plan"
+                planContents={AboutThisPlanCardContent}
+                aboutContents={AboutThisPlanContent}
+              />
+            </Box>
+            <Box my={{ base: 16, lg: 24 }}>
+              <DocumentSubmitted
+                title="Salary & House Property"
+                documents={Documents}
+              />
+            </Box>
+            <Box my={{ base: 16, lg: 24 }}>
+              <FAQSection
+                heading="Frequently Asked Questions"
+                servicesDatas={FAQs}
+              />
+            </Box>
+          </Container>
+          <BGWithQueriesForm />
+        </>
+      ) : (
+        Loading
+      )}
     </>
   );
 }
